4e3379751537ec02063e606f6ca9e37c1fa58f90
Krzysztof Miszewski <krzysztofxmiszewski@gmail.com>
2018-07-23 20:11:37 +0200

    maven + Word

---
+++ src/main/java/miszewski/jipang/service/impl/WordServiceImpl.java
@@ -0,0 +1,37 @@
+package miszewski.jipang.service.impl;
+
+import miszewski.jipang.model.Word;
+import miszewski.jipang.repository.WordRepository;
+import miszewski.jipang.service.WordService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class WordServiceImpl implements WordService {
+
+    private WordRepository wordRepository;
+
+    @Autowired
+    public WordServiceImpl(WordRepository wordRepository) {
+        this.wordRepository = wordRepository;
+    }
+
+    @Override
+    public void create(String polish, String japanese) {
+        Word word = new Word();
+        word.setPolish(polish);
+        word.setJapanese(japanese);
+    }
+
+    @Override
+    public Set<Word> findAllBy() {
+        return wordRepository.findAllBy();
+    }
+
+    @Override
+    public Set<Word> findById(Long bookId) {
+        return wordRepository.findById(bookId);
+    }
+}
